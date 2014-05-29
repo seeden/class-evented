5339c1cb218f8b78a56a62d0fe17ff4a46c1bbc0
Zlatko Fedor <zfedor@gmail.com>
2014-05-30 00:39:47 +0200

    new version of extender

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 
 	//AMD
 	if (typeof define !== 'undefined' && define.amd) {
-		define(['class-extend', 'class-emit'], function(Class, Emit) {
+		define(['class-extender', 'class-emit'], function(Class, Emit) {
 			return Class.extend({}, [Emit]);
 		});
 	}
 
 	//CommonJS
 	else if (typeof module !== 'undefined' && module.exports) {
-		var Class = require('class-extend');
+		var Class = require('class-extender');
 		var Emit = require('class-emit');
 
 		module.exports = Class.extend({}, [Emit]);
@@ -18,7 +18,7 @@
 
 	//Script tag
 	else {
-		root.ClassEmit = Class.extend({}, [Emit]);
+		root.ClassEvented = Class.extend({}, [Emit]);
 	}
 
 } (this));
